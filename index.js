99f7196f08be39fef87478223a16b10a3710ec9c
Daniel Egan <daniel.egan@microsoft.com>
2016-06-08 09:57:12 -0700

    fixing index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var path_1 = require('path');
+var RaspiCam = require('raspicam');
+var camera = new RaspiCam({
+    mode: 'photo',
+    timelapse: 1000,
+    output: path_1.join(__dirname, 'captures/%d')
+});
+camera.on("started", function () {
+    console.log('started taking photos every second (saved to captures directory)');
+});
+//when each photo is saved
+camera.on("read", function (e, f) { });
+//listen for the process to exit when the timeout has been reached
+camera.on("exited", function () { });
+//start taking timelapses
+camera.start();
+//# sourceMappingURL=index.js.map
