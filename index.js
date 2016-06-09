0733d394043720e6f5682d0ac9079a2c2ce405d7
Jeremy Foster <jeremy.foster@live.com>
2016-06-09 13:38:26 -0700

    clean up and refactor

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var path = require('path');
 var fs = require('fs');
 var request = require('request');
 var RaspiCam = require('raspicam');
-var needle = require('needle');
 var querystring = require('querystring');
 var camera = new RaspiCam({
     mode: 'photo',
-    timelapse: 10000,
+    timelapse: 1000,
     timeout: 0,
     rotation: 180,
     preview: '100,100,200,200',
@@ -21,49 +20,22 @@ camera.on("read", function (e, ts, f) {
     var params = querystring.stringify({
         "visualFeatures": "Tags"
     });
-    // console.log(f);
-    // var data = JSON.stringify({
-    //     file: 'captures/' + f,
-    //     content_type: 'image/jpg'
-    // });
-    // var options = {
-    //     headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-    //     multipart: true
-    // }
-    // needle.post('https://api.projectoxford.ai/vision/v1.0/analyze?' + params, data, options, function (err, resp) {
-    //     // you can pass params as a string or as an object.
-    //     console.log('err: ' + err);
-    //     console.log('msg: ' + err.message);
-    //     console.log(resp.status)
-    //     console.log(resp.body);
-    // });
-    var formData = {
-        my_file: fs.createReadStream(__dirname + '/captures/' + f),
-    };
-    request.post({
+    var options = {
         url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
         headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-        formData: formData
-    }, function (err, httpResponse, body) {
-        if (err)
-            return console.error('upload failed:', err);
-        console.log('Upload successful!  Server responded with:', body);
+        formData: {
+            my_file: fs.createReadStream(__dirname + '/captures/' + f),
+        }
+    };
+    request.post(options, function (err, httpResponse, body) {
+        console.log((err ? 'Error: ' + err : 'Success: ' + body));
     });
 });
-//listen for the process to exit when the timeout has been reached
-camera.on("exited", function () { });
 //start taking timelapses
 console.log('starting camera...');
 camera.start();
-function exit(x) {
-    console.log('x: ' + x);
-    console.log('stopping camera...');
-    camera.stop();
-}
-//do something when app is closing
-process.on('exit', exit);
-//catches ctrl+c event
-process.on('SIGINT', exit);
-//catches uncaught exceptions
-process.on('uncaughtException', exit);
+//catch crashes and unexpected exits
+process.on('exit', function () { return camera.stop(); });
+process.on('SIGINT', function () { return camera.stop(); });
+process.on('uncaughtException', function () { return camera.stop(); });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,12 +2,11 @@ import * as path from 'path';
 import * as fs from 'fs';
 var request = require('request');
 import RaspiCam = require('raspicam');
-var needle = require('needle');
 var querystring = require('querystring');
 
 let camera = new RaspiCam({
     mode: 'photo',
-    timelapse: 10000,
+    timelapse: 1000,
     timeout: 0,
     rotation: 180,
     preview: '100,100,200,200',
@@ -21,61 +20,29 @@ camera.on("started", () => {
 //when each photo is saved
 camera.on("read", (e, ts, f) => {
 
-    var params = querystring.stringify({
+    let params = querystring.stringify({
         "visualFeatures": "Tags"
     });
-    // console.log(f);
-    // var data = JSON.stringify({
-    //     file: 'captures/' + f,
-    //     content_type: 'image/jpg'
-    // });
 
-    // var options = {
-    //     headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-    //     multipart: true
-    // }
-
-    // needle.post('https://api.projectoxford.ai/vision/v1.0/analyze?' + params, data, options, function (err, resp) {
-    //     // you can pass params as a string or as an object.
-    //     console.log('err: ' + err);
-    //     console.log('msg: ' + err.message);
-    //     console.log(resp.status)
-    //     console.log(resp.body);
-    // });
-
-    var formData = {
-        my_file: fs.createReadStream(__dirname + '/captures/' + f),
-    };
-    request.post({
+    let options = {
         url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
         headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-        formData: formData
-    }, (err, httpResponse, body) => {
-        if (err) return console.error('upload failed:', err);
-        console.log('Upload successful!  Server responded with:', body);
+        formData: {
+            my_file: fs.createReadStream(__dirname + '/captures/' + f),
+        }
+    };
+
+    request.post(options, (err, httpResponse, body) => {
+        console.log((err ? 'Error: ' + err : 'Success: ' + body));
     });
 
 });
-
-//listen for the process to exit when the timeout has been reached
-camera.on("exited", () => { });
 
 //start taking timelapses
 console.log('starting camera...');
 camera.start();
 
-
-function exit(x) {
-    console.log('x: ' + x);
-    console.log('stopping camera...');
-    camera.stop();
-}
-
-//do something when app is closing
-process.on('exit', exit);
-
-//catches ctrl+c event
-process.on('SIGINT', exit);
-
-//catches uncaught exceptions
-process.on('uncaughtException', exit);
+//catch crashes and unexpected exits
+process.on('exit', () => camera.stop());
+process.on('SIGINT', () => camera.stop());
+process.on('uncaughtException', () => camera.stop());
