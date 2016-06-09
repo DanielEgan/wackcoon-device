aae5e8edf73a645f3fbe50cbf4480010a8c59747
Jeremy Foster <jeremy.foster@live.com>
2016-06-09 14:15:03 -0700

    don't send if filename contains ~

--- index.js
+++ index.js
@@ -2,34 +2,36 @@
 var path = require('path');
 var fs = require('fs');
 var request = require('request');
-var RaspiCam = require('raspicam');
 var querystring = require('querystring');
+var RaspiCam = require('raspicam');
 var camera = new RaspiCam({
     mode: 'photo',
     timelapse: 1000,
     timeout: 0,
     rotation: 180,
     preview: '100,100,200,200',
-    output: path.join(__dirname, 'captures/%d.jpg')
+    output: path.join(__dirname, 'captures', '%d.jpg')
 });
 camera.on("started", function () {
     console.log('started taking photos every second (saved to captures directory)');
 });
 //when each photo is saved
 camera.on("read", function (e, ts, f) {
-    var params = querystring.stringify({
-        "visualFeatures": "Tags"
-    });
-    var options = {
-        url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
-        headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-        formData: {
-            my_file: fs.createReadStream(__dirname + '/captures/' + f),
-        }
-    };
-    request.post(options, function (err, httpResponse, body) {
-        console.log((err ? 'Error: ' + err : 'Success: ' + body));
-    });
+    if (!f.search(/~/)) {
+        var params = querystring.stringify({
+            "visualFeatures": "Tags"
+        });
+        var options = {
+            url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
+            headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
+            formData: {
+                my_file: fs.createReadStream(path.join(__dirname, 'captures', f)),
+            }
+        };
+        request.post(options, function (err, httpResponse, body) {
+            console.log((err ? 'Error: ' + err : 'Success: ' + body));
+        });
+    }
 });
 //start taking timelapses
 console.log('starting camera...');
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
-var request = require('request');
+import * as request from 'request';
+import * as querystring from 'querystring';
 import RaspiCam = require('raspicam');
-var querystring = require('querystring');
 
 let camera = new RaspiCam({
     mode: 'photo',
@@ -10,7 +10,7 @@ let camera = new RaspiCam({
     timeout: 0,
     rotation: 180,
     preview: '100,100,200,200',
-    output: path.join(__dirname, 'captures/%d.jpg')
+    output: path.join(__dirname, 'captures', '%d.jpg')
 });
 
 camera.on("started", () => {
@@ -19,23 +19,23 @@ camera.on("started", () => {
 
 //when each photo is saved
 camera.on("read", (e, ts, f) => {
+    if (!f.search(/~/)) {
+        let params = querystring.stringify({
+            "visualFeatures": "Tags"
+        });
 
-    let params = querystring.stringify({
-        "visualFeatures": "Tags"
-    });
+        let options = {
+            url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
+            headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
+            formData: {
+                my_file: fs.createReadStream(path.join(__dirname, 'captures', f)),
+            }
+        };
 
-    let options = {
-        url: 'https://api.projectoxford.ai/vision/v1.0/analyze?' + params,
-        headers: { 'Ocp-Apim-Subscription-Key': '48cdc4d0cd6d4bed9f1cb05dcfef72ec', 'Content-Type': 'multipart/form-data' },
-        formData: {
-            my_file: fs.createReadStream(__dirname + '/captures/' + f),
-        }
-    };
-
-    request.post(options, (err, httpResponse, body) => {
-        console.log((err ? 'Error: ' + err : 'Success: ' + body));
-    });
-
+        request.post(options, (err, httpResponse, body) => {
+            console.log((err ? 'Error: ' + err : 'Success: ' + body));
+        });
+    }
 });
 
 //start taking timelapses
